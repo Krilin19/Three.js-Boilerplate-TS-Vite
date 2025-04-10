d6682427e97bbe922f0388157b2bec564a1359ea
Krilin19 <103615835+Krilin19@users.noreply.github.com>
2025-04-10 17:54:30 +1000

    first commit

--- src/main.ts
+++ src/main.ts
@@ -45,8 +45,8 @@ cameraFolder.open()
 function animate() {
   requestAnimationFrame(animate)
 
-  //cube.rotation.x += 0.01
-  //cube.rotation.y += 0.01
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
 
   renderer.render(scene, camera)
 
